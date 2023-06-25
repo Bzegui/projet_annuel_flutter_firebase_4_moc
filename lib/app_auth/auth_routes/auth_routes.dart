5e7adfc6964dab517cc8571e2ff6851155915c60
jean-baptiste abbate <jeanbaptisteabbate@outlook.fr>
2023-06-25 12:10:18 +0200

    update authenticated route

--- lib/app_auth/auth_routes/auth_routes.dart
+++ lib/app_auth/auth_routes/auth_routes.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/widgets.dart';
 import 'package:projet_annuel_flutter_firebase_4_moc/app_auth/app_auth_exports.dart';
 import 'package:projet_annuel_flutter_firebase_4_moc/login/login_exports.dart';
-import 'package:projet_annuel_flutter_firebase_4_moc/tab_views/chat_room/chat_room_exports.dart';
+import 'package:projet_annuel_flutter_firebase_4_moc/tabs/tabs_navigator/TabsNavigator.dart';
 
 List<Page<dynamic>> onGenerateAppAuthViewPages(
     AppAuthStatus state,
@@ -9,7 +9,7 @@ List<Page<dynamic>> onGenerateAppAuthViewPages(
     ) {
   switch (state) {
     case AppAuthStatus.authenticated:
-      return [ChatRoomPage.page()];
+      return [TabsNavigator.page()];
     case AppAuthStatus.unauthenticated:
       return [LoginPage.page()];
   }
