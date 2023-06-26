e31b469c12be82f98b09af43c268bf5e8c7d9956
jean-baptiste abbate <jeanbaptisteabbate@outlook.fr>
2023-06-26 18:56:32 +0200

    update file with code from tab pages navigator example

--- lib/tabs/tab_pages_navigator/tab_pages_navigator.dart
+++ lib/tabs/tab_pages_navigator/tab_pages_navigator.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:projet_annuel_flutter_firebase_4_moc/tabs/tab_views/chat_room/chat_room_exports.dart';
+import 'package:projet_annuel_flutter_firebase_4_moc/tabs/tab_views/contacts/contacts_page_exports.dart';
+import 'package:projet_annuel_flutter_firebase_4_moc/tabs/tab_views/settings/settings_page_exports.dart';
+
 import '../../app_auth/bloc/app_auth_bloc.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class TabPagesNavigator extends StatefulWidget {
   const TabPagesNavigator({Key? key}) : super(key: key);
@@ -13,57 +16,69 @@ class TabPagesNavigator extends StatefulWidget {
   State<TabPagesNavigator> createState() => _TabPagesNavigatorState();
 }
 
+TabBar get _tabBar => const TabBar(
+  labelColor: Colors.white,
+  tabs: <Widget>[
+    Tab(
+      icon: Icon(Icons.chat_bubble),
+      text: ('Chat'),
+
+    ),
+    Tab(
+      icon: Icon(Icons.contact_page),
+      text: 'Contacts',
+    ),
+    Tab(
+      icon: Icon(Icons.settings),
+      text: 'Settings',
+    ),
+  ],
+);
+
 class _TabPagesNavigatorState extends State<TabPagesNavigator> {
-  int _currentIndex = 0;
-  final _tabs = const [
-    ChatRoomPage(),
-  ];
 
   @override
   Widget build(BuildContext context) {
-    final textTheme = Theme.of(context).textTheme;
+    final textTheme = Theme
+        .of(context)
+        .textTheme;
     final user = context.select((AppAuthBloc bloc) => bloc.state.user);
+    const int tabsCount = 3;
 
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Chat'),
-        actions: <Widget>[
-          Align(
-            alignment: Alignment.center,
-            child: Text(user.email ?? '', textAlign: TextAlign.center,),
+    return DefaultTabController(
+      length: 3,
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Text('Chat'),
+          actions: <Widget>[
+            Align(
+              alignment: Alignment.center,
+              child: Text(user.email ?? '', textAlign: TextAlign.center,),
+            ),
+            IconButton(
+              key: const Key('homePage_logout_iconButton'),
+              icon: const Icon(Icons.exit_to_app),
+              onPressed: () {
+                context.read<AppAuthBloc>().add(const AppAuthLogoutRequested());
+              },
+            ),
+          ],
+          bottom: PreferredSize(
+            preferredSize: _tabBar.preferredSize,
+            child: Material(
+              color: Colors.indigoAccent, //<-- SEE HERE to mod tab bar color
+              child: _tabBar,
+            ),
           ),
-          IconButton(
-            key: const Key('homePage_logout_iconButton'),
-            icon: const Icon(Icons.exit_to_app),
-            onPressed: () {
-              context.read<AppAuthBloc>().add(const AppAuthLogoutRequested());
-            },
-          ),
-        ],
-      ),
-      body: IndexedStack(
-        index: _currentIndex,
-        children: _tabs,
-      ),
-      bottomNavigationBar: BottomNavigationBar(
-        currentIndex: _currentIndex,
-        onTap: _onTap,
-        items: const [
-          BottomNavigationBarItem(icon: Icon(Icons.chat_bubble),
-            label: 'Chat',
-          ),
-          BottomNavigationBarItem(icon: Icon(Icons.settings),
-            label: 'settings',
-          ),
-        ],
+        ),
+        body: const TabBarView(
+          children: <Widget>[
+            ChatRoomPage(),
+            ContactsPage(),
+            SettingsPage(),
+          ],
+        ),
       ),
     );
-  }
-
-  void _onTap(int index) {
-    setState(() {
-      _currentIndex = index;
-    });
-    debugPrint('current index $_currentIndex');
   }
 }
