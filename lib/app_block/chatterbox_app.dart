36cca10f069be15454794cbe96158e585b7873f5
jean-baptiste abbate <jeanbaptisteabbate@outlook.fr>
2023-06-28 13:21:51 +0200

    add multi bloc and repository providers, rename app

---
+++ lib/app_block/chatterbox_app.dart
@@ -0,0 +1,51 @@
+import 'package:authentication_repository/authentication_repository_exports.dart';
+import 'package:flow_builder/flow_builder.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:projet_annuel_flutter_firebase_4_moc/auth_block/app_auth/app_auth_exports.dart';
+import 'package:projet_annuel_flutter_firebase_4_moc/theme.dart';
+
+class ChatterboxApp extends StatelessWidget {
+  const ChatterboxApp({
+    required AuthenticationRepository authenticationRepository,
+    super.key,
+  }) : _authenticationRepository = authenticationRepository;
+  final AuthenticationRepository _authenticationRepository;
+
+  @override
+  Widget build(BuildContext context) {
+    return MultiRepositoryProvider(
+      providers: [
+        RepositoryProvider<AuthenticationRepository>(
+          create: (context) => _authenticationRepository,
+        )
+      ],
+      child: MultiBlocProvider(
+        providers: [
+          BlocProvider<AppAuthBloc>(
+            create: (_) => AppAuthBloc(
+              authenticationRepository: _authenticationRepository,
+            ),
+          )
+        ],
+        child: const ChatterboxAppView(),
+      ),
+    );
+  }
+}
+
+class ChatterboxAppView extends StatelessWidget {
+  const ChatterboxAppView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: theme,
+      home: FlowBuilder<AppAuthStatus>(
+        state: context.select((AppAuthBloc bloc) => bloc.state.status),
+        onGeneratePages: onGenerateAppAuthViewPages,
+      ),
+
+    );
+  }
+}
