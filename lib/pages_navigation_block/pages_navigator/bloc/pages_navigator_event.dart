c77cef5ebaae0903de1a141850e94456ec1afb4b
jean-baptiste abbate <jeanbaptisteabbate@outlook.fr>
2023-06-28 23:17:15 +0200

    rm pages navigation bloc and routes

--- lib/pages_navigation_block/pages_navigator/bloc/pages_navigator_event.dart
+++
@@ -1,7 +0,0 @@
-part of 'pages_navigator_bloc.dart';
-
-sealed class PagesNavigatorEvent {
-  const PagesNavigatorEvent();
-}
-
-final class OnPressedChatRoomActionButton extends PagesNavigatorEvent {}
