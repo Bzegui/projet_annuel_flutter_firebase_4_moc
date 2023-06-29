a8ac2aa408c16973375baa2222e989a8509cd54f
jean-baptiste abbate <jeanbaptisteabbate@outlook.fr>
2023-06-29 15:08:25 +0200

    add contacts option model

---
+++ packages/contacts_repository/lib/src/models/contacts_option.dart
@@ -0,0 +1,16 @@
+import 'package:equatable/equatable.dart';
+import 'package:flutter/cupertino.dart';
+
+class ContactsOption extends Equatable {
+
+  const ContactsOption({
+    required this.icon,
+    required this.label
+  });
+
+  final IconData icon;
+  final String label;
+
+  @override
+  List<Object> get props => [icon, label];
+}
