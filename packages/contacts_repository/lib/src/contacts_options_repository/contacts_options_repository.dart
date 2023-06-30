ad5111ffbeaa3984900ef0d46506d6c2c3c86471
jean-baptiste abbate <jeanbaptisteabbate@outlook.fr>
2023-06-30 09:51:34 +0200

    link data source with repository

--- packages/contacts_repository/lib/src/contacts_options_repository/contacts_options_repository.dart
+++ packages/contacts_repository/lib/src/contacts_options_repository/contacts_options_repository.dart
@@ -1,7 +1,21 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import '../models/contacts_option.dart';
+import '../data_sources/local_data_sources/contacts/contacts_options_data_source.dart';
 
-const defaultContactsOptions = <ContactsOption> [
-  ContactsOption(icon: Icons.person_add, label: 'Nouveau contact'),
-];
+class ContactsOptionsRepository {
+  final ContactsOptionsDataSource contactsOptionsDataSource;
+
+  ContactsOptionsRepository({
+    required this.contactsOptionsDataSource
+  });
+
+  Future<List<ContactsOption>> getContactsOptions() async {
+    try {
+      final contactsOptions = await contactsOptionsDataSource.getContactsOptions();
+      return contactsOptions;
+    } catch (e) {
+      rethrow;
+    }
+  }
+}
