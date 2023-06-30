327912f7f3e3a0ae2fb8a87c21114a6cae1946de
jean-baptiste abbate <jeanbaptisteabbate@outlook.fr>
2023-07-01 00:13:57 +0200

    add data source to contacts repository exports

--- packages/contacts_repository/lib/contacts_repository_exports.dart
+++ packages/contacts_repository/lib/contacts_repository_exports.dart
@@ -1,2 +1,3 @@
 export 'src/contacts_options_repository/contacts_options_repository.dart';
+export 'src/data_sources/local_data_sources/contacts/contacts_options_data_source.dart';
 export 'src/models/models_exports.dart';
