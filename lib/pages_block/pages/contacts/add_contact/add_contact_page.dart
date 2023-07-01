c6ed8177daaf4db52ae4224a4a0ef537bab722eb
jean-baptiste abbate <jeanbaptisteabbate@outlook.fr>
2023-07-01 22:36:50 +0200

    add contact and group creation pages

---
+++ lib/pages_block/pages/contacts/add_contact/add_contact_page.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class AddContactPage extends StatelessWidget {
+  const AddContactPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Text("add contact"),
+      ),
+    );
+  }
+}
