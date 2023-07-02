8b937b83058c526ecb789f1d8d02e4317f129f94
jean-baptiste abbate <jeanbaptisteabbate@outlook.fr>
2023-07-02 09:04:09 +0200

    add containers, box decoration and other UI design parameters

--- lib/pages_block/pages/contacts/contacts_options_items/contacts_options_item.dart
+++ lib/pages_block/pages/contacts/contacts_options_items/contacts_options_item.dart
@@ -1,8 +1,6 @@
 import 'package:contacts_repository/contacts_repository_exports.dart';
 import 'package:flutter/material.dart';
 
-import '../add_contact/add_contact_page.dart';
-
 class ContactOptionsItems extends StatelessWidget {
   final ContactsOptionsItem contactsOptionsItem;
 
@@ -13,20 +11,36 @@ class ContactOptionsItems extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ListTile(
-          title: Text(contactsOptionsItem.label),
-          leading: Icon(contactsOptionsItem.icon),
-          onTap: () {
-            Navigator.of(context).push(MaterialPageRoute(
-              builder: (routeContext) => contactsOptionsItem.navDestination,
-            ));
-          }
+    return Container(
+          padding: const EdgeInsets.only(top: 10),
+          child: ListTile(
+              title: Text(
+                contactsOptionsItem.label,
+                style: const TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 17,
+                ),
+              ),
+              leading: Container(
+                height: 41,
+                width: 41,
+                decoration: const BoxDecoration(
+                  color: Colors.indigoAccent,
+                  shape: BoxShape.circle,
+                ),
+                child: Center(
+                  child:  Icon(
+                    contactsOptionsItem.icon,
+                    color: const Color(0xFFE0F2F1),
+                  ),
+                ),
+              ),
+              onTap: () {
+                Navigator.of(context).push(MaterialPageRoute(
+                  builder: (routeContext) => contactsOptionsItem.navDestination,
+                ));
+              }
+          ),
       );
-  }
-
-  void _onContactsOptionsItemNavigationDestination(BuildContext context) {
-    Navigator.of(context).push(MaterialPageRoute(
-      builder: (routeContext) => contactsOptionsItem.navDestination,
-    ));
   }
 }
