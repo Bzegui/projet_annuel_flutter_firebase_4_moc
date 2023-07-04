d5349f37129117b53fd8d0e4b9ce33f18a2df7db
jean-baptiste abbate <jeanbaptisteabbate@outlook.fr>
2023-07-04 15:28:29 +0200

    add contact ID input, button

--- lib/pages_block/pages/contacts/add_contact/view/add_contact_form.dart
+++ lib/pages_block/pages/contacts/add_contact/view/add_contact_form.dart
@@ -8,24 +8,81 @@ class AddContactForm extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    return BlocListener<AddContactCubit, AddContactState>(
+      listener: (context, state) {
+        if (state.status.isSuccess) {
+          Navigator.of(context).pop();
+        } else if (state.status.isFailure) {
+          ScaffoldMessenger.of(context)
+            ..hideCurrentSnackBar()
+            ..showSnackBar(
+              SnackBar(content: Text(state.errorMessage ?? 'Failed adding contact')),
+            );
+        }
+      },
+      child: Align(
+        alignment: const Alignment(0, -1 / 3),
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            const SizedBox(height: 8),
+            _ContactIdInput(),
+            _AddContactButton(),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class _ContactIdInput extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return BlocBuilder<AddContactCubit, AddContactState>(
+        buildWhen: (previous, current) => previous.contactId
+            != current.contactId,
+        builder: (context, state) {
+          return TextField(
+            key: const Key('addContactForm_contactIdInput_textField'),
+            onChanged: (contactId) => context.read<AddContactCubit>()
+                .contactIdChanged(contactId),
+            keyboardType: TextInputType.text,
+            decoration: InputDecoration(
+              labelText: 'contactId',
+              helperText: '',
+              errorText:
+                state.contactId.displayError != null ?
+                'invalid contact ID' : null
+            ),
+          );
+        },
+    );
+  }
+}
+
+
+class _AddContactButton extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
     return BlocBuilder<AddContactCubit, AddContactState>(
         builder: (context, state) {
           return state.status.isInProgress
               ? const CircularProgressIndicator()
               : ElevatedButton(
-            key: const Key('signUpForm_continue_raisedButton'),
-            style: ElevatedButton.styleFrom(
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(30),
-              ),
-              backgroundColor: Colors.deepPurpleAccent,
-            ),
-            onPressed: state.isValid
-                ? () => context.read<AddContactCubit>().addContactFormSubmitted()
-                : null,
-            child: const Text('ADD'),
+                  key: const Key('addContactForm_continue_raisedButton'),
+                  style: ElevatedButton.styleFrom(
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(30),
+                    ),
+                    backgroundColor: Colors.indigoAccent,
+                  ),
+                  onPressed: state.isValid
+                      ? () => context.read<AddContactCubit>()
+                      .addContactFormSubmitted() : null,
+                  child: const Text('ADD'),
           );
         }
     );
   }
 }
+
