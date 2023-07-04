d79ff8632ee33f0b1b9440ed9dcfa27d43cba76f
jean-baptiste abbate <jeanbaptisteabbate@outlook.fr>
2023-07-04 08:04:17 +0200

    update exports

--- packages/contacts_repository/lib/contacts_repository_exports.dart
+++ packages/contacts_repository/lib/contacts_repository_exports.dart
@@ -1,3 +1,5 @@
 export 'src/models/models_exports.dart';
 export 'src/contacts_options_items_repository/contacts_options_items_repository.dart';
-export 'src/data_sources/local_data_sources/contacts/contacts_options_items_data_source.dart';
+export 'src/contacts_repository/contacts_repository.dart';
+export 'src/data_sources/local_data_sources/contacts/contacts_local_data_source.dart';
+export 'src/data_sources/remote_data_sources/contacts/contacts_remote_data_source.dart';
