f7aabea6ae7481a2d1abf20de3d324272c9c184a
jean-baptiste abbate <jeanbaptisteabbate@outlook.fr>
2023-07-04 08:00:02 +0200

    add contact model

---
+++ packages/contacts_repository/lib/src/models/contacts/contact.dart
@@ -0,0 +1,45 @@
+import 'package:equatable/equatable.dart';
+
+/// {@template contact}
+/// Contact model
+///
+/// {@endtemplate}
+class Contact extends Equatable {
+  /// {@macro contact}
+  const Contact({
+    required this.id,
+    required this.contactId,
+    this.name,
+    this.email,
+    this.photo,
+  });
+
+  /// The current contact's id.
+  final String id;
+
+  /// The current contact's contactId.
+  final String contactId;
+
+  /// The current contact's name (display name).
+  final String? name;
+
+  /// The current contact's email address.
+  final String? email;
+
+  /// contact photo URL.
+  final String? photo;
+
+  /// contact factory for getting contacts list from Firestore.
+  factory Contact.fromFirestore(Map<String, dynamic> map, String id) {
+    return Contact(
+      id: id,
+      contactId: map['contact_id'],
+      name: map['name'],
+      email: map['email'],
+      photo: map['photo'],
+    );
+  }
+
+  @override
+  List<Object?> get props => [id, contactId, name, email, photo];
+}
