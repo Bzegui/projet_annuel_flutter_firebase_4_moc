31bd3aeba91d646422208ddda5e62ca658ca9859
jean-baptiste abbate <jeanbaptisteabbate@outlook.fr>
2023-07-04 08:35:48 +0200

    update imports

--- lib/app_block/chatterbox_app.dart
+++ lib/app_block/chatterbox_app.dart
@@ -6,11 +6,6 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:projet_annuel_flutter_firebase_4_moc/auth_block/app_auth/app_auth_exports.dart';
 import 'package:projet_annuel_flutter_firebase_4_moc/theme.dart';
 
-import '../pages_block/pages/contacts/add_contact/cubit/add_contact_cubit.dart';
-import '../pages_block/pages/contacts/add_contact/view/add_contact_form.dart';
-import '../pages_block/pages/contacts/add_contact/view/add_contact_page.dart';
-import '../pages_block/pages/contacts/bloc/contacts_bloc.dart';
-
 class ChatterboxApp extends StatelessWidget {
   const ChatterboxApp({
     required AuthenticationRepository authenticationRepository,
@@ -38,11 +33,6 @@ class ChatterboxApp extends StatelessWidget {
               authenticationRepository: _authenticationRepository,
             ),
           ),
-          BlocProvider<ContactsBloc>(
-            create: (context) => ContactsBloc(
-              contactsRepository: RepositoryProvider.of<ContactsRepository>(context),
-            ),
-          )
         ],
         child: const ChatterboxAppView(),
       ),
