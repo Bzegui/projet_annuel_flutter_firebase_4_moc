fad013d763d0483bbab8c6eff28d9bba2f89770f
jean-baptiste abbate <jeanbaptisteabbate@outlook.fr>
2023-07-04 08:35:57 +0200

    update imports

--- lib/pages_block/pages/contacts/contacts_page.dart
+++ lib/pages_block/pages/contacts/contacts_page.dart
@@ -1,12 +1,7 @@
 import 'package:contacts_repository/contacts_repository_exports.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:projet_annuel_flutter_firebase_4_moc/pages_block/pages/contacts/add_contact/cubit/add_contact_cubit.dart';
-import 'package:projet_annuel_flutter_firebase_4_moc/pages_block/pages/contacts/add_contact/view/add_contact_page.dart';
 import 'package:projet_annuel_flutter_firebase_4_moc/pages_block/pages/contacts/contacts_options_list/contacts_options_list.dart';
-import 'add_contact/view/add_contact_form.dart';
-import 'bloc/contacts_bloc.dart';
-import 'contacts_options_list/contacts_options_list.dart';
 
 class ContactsPage extends StatelessWidget {
 
