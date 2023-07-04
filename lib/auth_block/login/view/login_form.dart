31fc023cf491cc05805ff6886957735d8b732a8d
jean-baptiste abbate <jeanbaptisteabbate@outlook.fr>
2023-07-04 15:29:37 +0200

    update import

--- lib/auth_block/login/view/login_form.dart
+++ lib/auth_block/login/view/login_form.dart
@@ -1,4 +1,4 @@
-import 'package:flutter/material.dart';
+ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:projet_annuel_flutter_firebase_4_moc/auth_block/login/login_exports.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
