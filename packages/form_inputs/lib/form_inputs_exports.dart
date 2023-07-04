ba1003132bf88db3c19ff15e4933f22dc07791c2
jean-baptiste abbate <jeanbaptisteabbate@outlook.fr>
2023-07-04 08:02:14 +0200

    update exports

--- packages/form_inputs/lib/form_inputs_exports.dart
+++ packages/form_inputs/lib/form_inputs_exports.dart
@@ -1,3 +1,4 @@
-export '../src/inputs_models/confirmed_password.dart';
-export '../src/inputs_models/email.dart';
-export '../src/inputs_models/password.dart';
+export 'src/inputs_models/app_auth/confirmed_password.dart';
+export 'src/inputs_models/app_auth/email.dart';
+export 'src/inputs_models/app_auth/password.dart';
+export 'src/inputs_models/contacts/contact_id.dart';
