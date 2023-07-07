2f5ce67eeeb34801437ba19eb0e5b9860bf9eda7
jean-baptiste abbate <jeanbaptisteabbate@outlook.fr>
2023-07-07 08:24:52 +0200

    update list view render, update switch case for bloc builder

--- lib/pages_block/pages/contacts/add_contact/view/add_contact_form.dart
+++ lib/pages_block/pages/contacts/add_contact/view/add_contact_form.dart
@@ -1,18 +1,49 @@
+import 'package:contacts_repository/contacts_repository_exports.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:formz/formz.dart';
-import 'package:projet_annuel_flutter_firebase_4_moc/pages_block/pages/contacts/add_contact/cubit/add_contact_cubit.dart';
+import 'package:projet_annuel_flutter_firebase_4_moc/pages_block/pages/contacts/bloc/contacts_bloc.dart';
 
-class AddContactForm extends StatelessWidget {
+import '../../contact_item/contact_item.dart';
+
+class AddContactForm extends StatefulWidget {
   const AddContactForm({Key? key}) : super(key: key);
 
   @override
+  State<AddContactForm> createState() => _AddContactFormState();
+}
+
+class _AddContactFormState extends State<AddContactForm> {
+  @override
+  void didChangedDependencies() {
+    super.didChangeDependencies();
+  }
+
+ /* @override
   Widget build(BuildContext context) {
-    return BlocListener<AddContactCubit, AddContactState>(
+    return Padding(
+      padding: const EdgeInsets.all(25),
+      child: BlocBuilder<ContactsBloc, ContactsState>(
+        builder: (context, state) {
+          switch (state.contactsStatus) {
+            case ContactsStatus.fetchingContacts:
+              return const Center(child: CircularProgressIndicator());
+            case ContactsStatus.fetchedContacts:
+              return _buildContactsList(context, state.contacts);
+            default:
+              return _buildContactsList(context, state.contacts);
+          }
+        },
+      ),
+    );
+  }*/
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocListener<ContactsBloc, ContactsState>(
       listener: (context, state) {
-        if (state.status.isSuccess) {
-          Navigator.of(context).pop();
-        } else if (state.status.isFailure) {
+        if (state.addContactStatus.isSuccess) {
+        } else if (state.addContactStatus.isFailure) {
           ScaffoldMessenger.of(context)
             ..hideCurrentSnackBar()
             ..showSnackBar(
@@ -21,66 +52,100 @@ class AddContactForm extends StatelessWidget {
         }
       },
       child: Align(
-        alignment: const Alignment(0, -1 / 3),
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            const Padding(
-              padding: EdgeInsets.only(bottom: 8),
-              child:  SizedBox(
-                height: 60,
-                child: Text(
-                  "Ajouter un contact",
-                  style: TextStyle(
-                    fontWeight: FontWeight.bold,
-                    fontSize: 25,
-                  )
+          alignment: const Alignment(0, -1 / 3),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              const Padding(
+                padding: EdgeInsets.only(bottom: 8),
+                child:  SizedBox(
+                  height: 60,
+                  child: Text(
+                      "Ajouter un contact",
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 25,
+                      )
+                  ),
+                  //
                 ),
-                //
               ),
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: <Widget>[
-                Padding(
-                  padding: const EdgeInsets.all(0),
-                  child:  SizedBox(
-                    height: 47, width: 140,
-                    child: _AddContactButton(),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Padding(
+                    padding: const EdgeInsets.only(left: 8, bottom: 5),
+                    child:  SizedBox(
+                      height: 70, width: 200,
+                      child: _ContactIdInput(),
+                    ),
                   ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.only(left: 8, bottom: 5),
-                  child:  SizedBox(
-                    height: 70, width: 200,
-                    child: _ContactIdInput(),
-                  ),
-                ),
-              ],
-            ),
-            const Divider(
-              color: Colors.grey,
-              height: 1,
-            ),
-          ],
-        )
+                ],
+              ),
+              const Divider(
+                color: Colors.grey,
+                height: 1,
+              ),
+
+              BlocBuilder<ContactsBloc, ContactsState>(
+                builder: (context, state) {
+                  /*switch (state.addContactStatus) {
+                    case FormzSubmissionStatus.inProgress:
+                      return const Center(child: CircularProgressIndicator());
+                    case FormzSubmissionStatus.success:
+                      return _buildContactsList(context, state.contacts);
+                    case FormzSubmissionStatus.failure:
+
+                    default:
+                      return _buildContactsList(context, state.contacts);
+                  }*/
+                  switch (state.contactsStatus) {
+                    case ContactsStatus.fetchingContacts:
+                      return const Center(child: CircularProgressIndicator());
+                    case ContactsStatus.fetchedContacts:
+                      return _buildContactsList(context, state.contacts);
+                    default:
+                      return _buildContactsList(context, state.contacts);
+                  }
+                },
+              )
+            ],
+          )
       ),
+    );
+  }
+
+  Widget _buildContactsList(BuildContext context, List<Contact> contacts) {
+    if (contacts.isEmpty) {
+      return const Center(child: Text('No contacts'));
+    }
+    return ListView.builder(
+      shrinkWrap: true,
+      itemCount: contacts.length,
+      itemBuilder: (context, index) {
+        final contact = contacts[index];
+        return ContactItem(
+          contact: contact,
+        );
+      },
     );
   }
 }
 
+
 class _ContactIdInput extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return BlocBuilder<AddContactCubit, AddContactState>(
+    return BlocBuilder<ContactsBloc, ContactsState>(
         buildWhen: (previous, current) => previous.contactId
             != current.contactId,
         builder: (context, state) {
           return TextField(
             key: const Key('addContactForm_contactIdInput_textField'),
-            onChanged: (contactId) => context.read<AddContactCubit>()
-                .contactIdChanged(contactId),
+            onChanged: (contactId) => context.read<ContactsBloc>()
+                .add(AddContactById(contactId)), /*context.read<ContactsBloc>()
+                .add(GetAllContacts()),*/
             keyboardType: TextInputType.text,
             decoration: InputDecoration(
               labelText: 'contactId',
@@ -91,31 +156,6 @@ class _ContactIdInput extends StatelessWidget {
             ),
           );
         },
-    );
-  }
-}
-
-class _AddContactButton extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return BlocBuilder<AddContactCubit, AddContactState>(
-        builder: (context, state) {
-          return state.status.isInProgress
-              ? const CircularProgressIndicator()
-              : ElevatedButton(
-                  key: const Key('addContactForm_continue_raisedButton'),
-                  style: ElevatedButton.styleFrom(
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(30),
-                    ),
-                    backgroundColor: Colors.indigoAccent,
-                  ),
-                  onPressed: state.isValid
-                      ? () => context.read<AddContactCubit>()
-                      .addContactFormSubmitted() : null,
-                  child: const Text('AJOUTER'),
-          );
-        }
     );
   }
 }
