d000267f9738922a850a8d1a6918f9a74d1bbaa0
jean-baptiste abbate <jeanbaptisteabbate@outlook.fr>
2023-07-07 10:10:59 +0200

    update indent

--- lib/pages_block/pages/contacts/contacts_options_items/bloc/contacts_options_items_bloc.dart
+++ lib/pages_block/pages/contacts/contacts_options_items/bloc/contacts_options_items_bloc.dart
@@ -5,18 +5,23 @@ import 'package:meta/meta.dart';
 part 'contacts_options_items_event.dart';
 part 'contacts_options_items_state.dart';
 
-class ContactsOptionsItemsBloc extends Bloc<ContactsOptionsItemsEvent, ContactsOptionsItemsRepositoryState> {
+class ContactsOptionsItemsBloc extends Bloc<ContactsOptionsItemsEvent,
+    ContactsOptionsItemsRepositoryState> {
   final ContactsOptionsItemsRepository contactsOptionsRepository;
 
-  ContactsOptionsItemsBloc({required this.contactsOptionsRepository}) : super(ContactsOptionsItemsRepositoryState()) {
+  ContactsOptionsItemsBloc({required this.contactsOptionsRepository})
+      : super(ContactsOptionsItemsRepositoryState()) {
     on<GetAllContactsOptionsItems>((event, emit) async {
       emit(state.copyWith(status: ContactsOptionsItemsStatus.loading));
 
       try {
-        final contactsOptionsItems = await contactsOptionsRepository.getContactsOptions();
-        emit(state.copyWith(status: ContactsOptionsItemsStatus.success, contactsOptionsItem: contactsOptionsItems));
+        final contactsOptionsItems =
+        await contactsOptionsRepository.getContactsOptions();
+        emit(state.copyWith(status: ContactsOptionsItemsStatus.success,
+            contactsOptionsItem: contactsOptionsItems));
       } catch(error) {
-        emit(state.copyWith(status: ContactsOptionsItemsStatus.error, error: error.toString()));
+        emit(state.copyWith(status: ContactsOptionsItemsStatus.error,
+            error: error.toString()));
       }
     });
   }
