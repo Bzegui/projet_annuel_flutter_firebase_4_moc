d7f3c72d3c2516a3ca2babe29d6922eb683afe4b
jean-baptiste abbate <jeanbaptisteabbate@outlook.fr>
2023-07-09 23:47:39 +0200

    add exports

---
+++ lib/pages_block/pages/contacts/contacts_menu/components/components_exports.dart
@@ -0,0 +1,4 @@
+export 'contact_item/contact_item.dart';
+export 'contacts_options_item/contacts_options_item.dart';
+export 'contacts_options_list/contacts_options_list.dart';
+export 'contact_items_list/contact_items_list.dart';
